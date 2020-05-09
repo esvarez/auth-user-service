d7e27edc503e0757ea45f75e8fef4a4f951779f8
Erick Suarez <ericksuarezdev@gmail.com>
2020-05-09 14:13:54 -0500

    Compile successfully

--- src/app/modules/writer/writer.module.ts
+++ src/app/modules/writer/writer.module.ts
@@ -1,24 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import {NzButtonModule, NzFormModule, NzInputModule, NzLayoutModule, NzListModule, NzMenuModule, NzModalModule} from 'ng-zorro-antd';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import {IconsProviderModule} from '../icons-provider/icons-provider.module';
-import { CategoriesComponent } from './pages/categories/categories.component';
-import {AppRoutingModule} from '../../app-routing.module';
-import { CategoryListComponent } from './components/category-list/category-list.component';
-import { AddCategoryComponent } from './components/add-category/add-category.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-=======
-import {NzLayoutModule, NzMenuModule} from 'ng-zorro-antd';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import {IconsProviderModule} from '../icons-provider/icons-provider.module';
->>>>>>> master
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { NzButtonModule, NzFormModule, NzInputModule, NzLayoutModule, NzListModule, NzMenuModule, NzModalModule} from 'ng-zorro-antd'
+import { DashboardComponent } from './pages/dashboard/dashboard.component'
+import { IconsProviderModule} from '../icons-provider/icons-provider.module'
+import { CategoriesComponent } from './pages/categories/categories.component'
+import { AppRoutingModule} from '../../app-routing.module'
+import { CategoryListComponent } from './components/category-list/category-list.component'
+import { AddCategoryComponent } from './components/add-category/add-category.component'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 
 
 @NgModule({
-<<<<<<< HEAD
   declarations: [DashboardComponent, CategoriesComponent, CategoryListComponent, AddCategoryComponent],
   imports: [
     CommonModule,
@@ -33,14 +26,6 @@ import {IconsProviderModule} from '../icons-provider/icons-provider.module';
     NzListModule,
     NzMenuModule,
     NzModalModule,
-=======
-  declarations: [DashboardComponent],
-  imports: [
-    CommonModule,
-    NzLayoutModule,
-    NzMenuModule,
-    IconsProviderModule
->>>>>>> master
   ]
 })
 export class WriterModule { }
